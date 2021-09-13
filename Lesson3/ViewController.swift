4dee12f2a2f4557e8149da924c0b5b064a34cad5
9BristolMyers <88287985+9BristolMyers@users.noreply.github.com>
2021-09-13 16:27:52 +0300

    Initial Commit

---
+++ Lesson3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson3
+//
+//  Created by Владимир Рузавин on 9/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
